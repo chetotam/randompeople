f465cbe3435a44594084973f4da6caa344340d2e
Denis <pogonchenkod@gmail.com>
2017-06-17 23:15:02 +0300

    Add shell context initialization

--- manage.py
+++ manage.py
@@ -1,9 +1,15 @@
 # TODO: migrate to Flask CLI instead of Flask-Script
 '''This module enables command line interface for randompeople app.'''
 from flask_script import Manager
-from app import create_app
+from app import create_app, db
+from app.models import Room, Member
 
 manager = Manager(create_app)
+
+@manager.shell
+def make_shell_context():
+    ''''''
+    return dict(app=manager.app, db=db, Room=Room, Member=Member)
 
 if __name__ == '__main__':
     manager.run()
