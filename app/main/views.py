afdef1ed99a0b00f2314b1d04c7e402b0735f313
Denis <pogonchenkod@gmail.com>
2017-06-19 00:20:17 +0300

    Accept POST requests from main page

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,10 @@
 '''This module contains handler for main page of the application.'''
 from flask import current_app
+
 from . import main
 
-@main.route('/')
+
+@main.route('/', methods=['GET', 'POST'])
 def index():
     '''Return main page.'''
     return current_app.send_static_file('index.html')
