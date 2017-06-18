5a3d94448b62bd248e8413b16be2e98a01b4fb24
Denis <pogonchenkod@gmail.com>
2017-06-19 00:32:21 +0300

    nothing

--- app/api/v1/rooms.py
+++ app/api/v1/rooms.py
@@ -11,7 +11,7 @@ class Rooms(MethodView):
 
     def get(self, room_name):
         '''Return specified room as JSON.'''
-        Room(name='test', members=[])
+        Room(name='test', members=[]).save()
         if room_name is None:
             return jsonify(Room.objects), 200
         else:
